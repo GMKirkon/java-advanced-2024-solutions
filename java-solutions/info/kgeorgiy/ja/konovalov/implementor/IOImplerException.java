3732b5a20b09dadf4fbdf4e33bf23f7e839fbecc
GMKirkon <367307@edu.itmo.ru>
2024-03-29 10:26:39 +0300

    upd hw4-6

--- java-solutions/info/kgeorgiy/ja/konovalov/implementor/IOImplerException.java
+++ java-solutions/info/kgeorgiy/ja/konovalov/implementor/IOImplerException.java
@@ -2,7 +2,15 @@ package info.kgeorgiy.ja.konovalov.implementor;
 
 import info.kgeorgiy.java.advanced.implementor.ImplerException;
 
+/**
+ * Helper exception to separate exception hierarchy.
+ * Indicates that the problem in {@link Implementor} happened due to IO
+ */
 public class IOImplerException extends ImplerException {
+    /**
+     * Constructs IO Implementor Exception with a provided message
+     * @param message error message
+     */
     public IOImplerException(String message) {
         super("During IO happened problem " + message);
     }
