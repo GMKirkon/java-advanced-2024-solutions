014929c5267ce334c38accb1ece904790bc5b0e6
GMKirkon <367307@edu.itmo.ru>
2024-03-29 16:17:16 +0300

    jar + javadoc upd

--- scripts/jarGenerator.sh
+++ scripts/jarGenerator.sh
@@ -1,7 +1,7 @@
 path=$(cd ..; pwd)
 path_to_shared_code=$(cd ../../java-advanced-2024; pwd)
 
-shared_implementor=$path_to_shared_code/modules/info.kgeorgiy.java.advanced.implementor
+shared_implementor="$path_to_shared_code"/modules/info.kgeorgiy.java.advanced.implementor
 
 javac -d "$(pwd)" \
   "$path"/java-solutions/info/kgeorgiy/ja/konovalov/implementor/Implementor.java\
--- scripts/javadocGenerator.sh
+++ scripts/javadocGenerator.sh
@@ -1,7 +1,7 @@
 path=$(cd ..; pwd)
 path_to_shared_code=$(cd ../../java-advanced-2024; pwd)
 
-shared_implementor=$path_to_shared_code/modules/info.kgeorgiy.java.advanced.implementor
+shared_implementor="$path_to_shared_code"/modules/info.kgeorgiy.java.advanced.implementor
 
 javadoc -package -private -d "$path/javadocDirectory"\
   "$path"/java-solutions/info/kgeorgiy/ja/konovalov/implementor/*.java\
